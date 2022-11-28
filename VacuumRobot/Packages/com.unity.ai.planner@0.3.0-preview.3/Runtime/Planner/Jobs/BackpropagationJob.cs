4b53de49fccf7143e77f278e75fdb44120759bb2
Lukas Reuter <lukasreuter@outlook.com>
2022-11-28 20:01:12 +0100

    Fixed: updated burst to get the allocation bug fixed

--- VacuumRobot/Packages/com.unity.ai.planner@0.3.0-preview.3/Runtime/Planner/Jobs/BackpropagationJob.cs
+++ VacuumRobot/Packages/com.unity.ai.planner@0.3.0-preview.3/Runtime/Planner/Jobs/BackpropagationJob.cs
@@ -171,8 +171,7 @@ namespace Unity.AI.Planner.Jobs
                 nextHorizon.Clear();
             }
 
-            //TODO: if this is explicitly disposed while burst compiled it leads to the UnsafeUtility::Free error
-            // currentHorizon.Dispose();
+            currentHorizon.Dispose();
             nextHorizon.Dispose();
             statesToUpdate.Dispose();
         }
